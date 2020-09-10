92aff0d030114bd6d7a21462b0b9d65b079ba242
tatiana <khen.tatiana@gmail.com>
2020-09-09 23:07:33 -0700

    "Hello, Wold" is added

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.endEditing(true)
-        print("Hello, World!")
+        print("Hello, World")
     }
 
 
