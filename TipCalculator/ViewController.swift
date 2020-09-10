4aae981e0054d621dc201ba5833bae524382aeb6
tatiana <khen.tatiana@gmail.com>
2020-09-09 22:56:43 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Tatiana on 9/9/20.
+//  Copyright © 2020 Tatiana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
